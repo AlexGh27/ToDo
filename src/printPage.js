07653afebbe64d87fcc32a88d50dcab744cf9806
AlexGh27 <alexghioca27@gmail.com>
2022-10-18 19:53:13 +0300

    Building the homepage

--- src/printPage.js
+++ src/printPage.js
@@ -1,3 +1,8 @@
 export default function printMe() {
     
+    const container = document.querySelector(".container");
+    const titleBar = document.createElement("div");
+    titleBar.className = "titleBar";
+    titleBar.innerHTML = "TO DO LIST";
+    container.appendChild(titleBar);
   }
