994011c407d65b5c71bbbbf32be55a0fa1a44ad8
AlexGh27 <alexghioca27@gmail.com>
2022-10-19 10:41:40 +0300

    Created the first 3 buttons

--- src/printPage.js
+++ src/printPage.js
@@ -11,6 +11,46 @@ export default function printMe() {
     container.appendChild(sidebar);
 
     const inbox = document.createElement("div");
-    inbox.className = "inbox";
+    inbox.className = "sideButton";
     sidebar.appendChild(inbox);
+  
+    const inboxImage = document.createElement("img");
+    inboxImage.className = "icon";
+    inboxImage.src = "inbox.png";
+    inbox.appendChild(inboxImage);
+
+    const inboxText = document.createElement("div");
+    inboxText.className = "text";
+    inboxText.innerHTML = "Inbox"
+    inbox.appendChild(inboxText);
+
+
+    const today = document.createElement("div");
+    today.className = "sideButton";
+    sidebar.appendChild(today);
+  
+    const todayImage = document.createElement("img");
+    todayImage.className = "icon";
+    todayImage.src = "today.png";
+    today.appendChild(todayImage);
+
+    const todayText = document.createElement("div");
+    todayText.className = "text";
+    todayText.innerHTML = "Today"
+    today.appendChild(todayText);
+
+
+    const week = document.createElement("div");
+    week.className = "sideButton";
+    sidebar.appendChild(week);
+  
+    const weekImage = document.createElement("img");
+    weekImage.className = "icon";
+    weekImage.src = "week.png";
+    week.appendChild(weekImage);
+
+    const weekText = document.createElement("div");
+    weekText.className = "text";
+    weekText.innerHTML = "This Week"
+    week.appendChild(weekText);
   }
